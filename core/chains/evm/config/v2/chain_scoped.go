fishdev2/chainlink

Add FinalityTag config

core/chains/evm/config/v2/chain_scoped.go
@@ -109,6 +109,10 @@ func (c *ChainScoped) EvmFinalityDepth() uint32 {
 	return *c.cfg.FinalityDepth
 }
 
+func (c *ChainScoped) EvmFinalityTag() bool {
+	return *c.cfg.FinalityTag
+}
+
 func (c *ChainScoped) EvmGasBumpPercent() uint16 {
 	return *c.cfg.GasEstimator.BumpPercent
 }

core/chains/evm/gas/helpers_test.go
@@ -139,6 +139,10 @@ func (m *MockConfig) EvmFinalityDepth() uint32 {
 	panic("not implemented") // TODO: Implement
 }
 
+func (m *MockConfig) EvmFinalityTag() bool {
+	panic("not implemented") // TODO: Implement
+}
+
 func (m *MockConfig) EvmGasBumpPercent() uint16 {
 	return m.EvmGasBumpPercentF
 }

core/chains/evm/gas/mocks/config.go
@@ -56,6 +56,20 @@ func (_m *Config) EvmFinalityDepth() uint32 {
 	return r0
 }
 
+// EvmFinalityTag provides a mock function with given fields:
+func (_m *Config) EvmFinalityTag() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 // EvmGasBumpPercent provides a mock function with given fields:
 func (_m *Config) EvmGasBumpPercent() uint16 {
 	ret := _m.Called()

core/chains/evm/txmgr/mocks/config.go
@@ -58,6 +58,20 @@ func (_m *Config) EvmFinalityDepth() uint32 {
 	return r0
 }
 
+// EvmFinalityTag provides a mock function with given fields:
+func (_m *Config) EvmFinalityTag() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 // EvmGasBumpPercent provides a mock function with given fields:
 func (_m *Config) EvmGasBumpPercent() uint16 {
 	ret := _m.Called()

cb-defi-notifs/smartcontractkit-chainlink-ad0a3279

core/chains/evm/config/v2/chain_scoped.go
@@ -98,6 +98,10 @@ func (e *evmConfig) FinalityDepth() uint32 {
 	return *e.c.FinalityDepth
 }
 
+func (e *evmConfig) FinalityTag() bool {
+	return *e.c.FinalityTag
+}
+
 func (e *evmConfig) LogKeepBlocksDepth() uint32 {
 	return *e.c.LogKeepBlocksDepth
 }

core/chains/evm/gas/helpers_test.go
@@ -125,6 +125,10 @@ func (m *MockConfig) FinalityDepth() uint32 {
 	panic("not implemented") // TODO: Implement
 }
 
+func (m *MockConfig) FinalityTag() bool {
+	panic("not implemented") // TODO: Implement
+}
+
 type MockGasEstimatorConfig struct {
 	EIP1559DynamicFeesF bool
 	BumpPercentF        uint16

core/chains/evm/gas/mocks/config.go
@@ -41,6 +41,20 @@ func (_m *Config) FinalityDepth() uint32 {
 	return r0
 }
 
+// FinalityTag provides a mock function with given fields:
+func (_m *Config) FinalityTag() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 type mockConstructorTestingTNewConfig interface {
 	mock.TestingT
 	Cleanup(func())

core/chains/evm/txmgr/mocks/config.go
@@ -44,6 +44,20 @@ func (_m *Config) FinalityDepth() uint32 {
 	return r0
 }
 
+// FinalityTag provides a mock function with given fields:
+func (_m *Config) FinalityTag() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 // KeySpecificMaxGasPriceWei provides a mock function with given fields: addr
 func (_m *Config) KeySpecificMaxGasPriceWei(addr common.Address) *assets.Wei {
 	ret := _m.Called(addr)